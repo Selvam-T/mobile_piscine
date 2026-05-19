793c25e4cfbef057d26e6197b9bddea623a8ffb8
Selvam-AI <selvam.thiagarajan@outlook.com>
2026-05-19 16:08:10 +0800

    first after moving to vmm

---
+++ mobileModule00/ex00/lib/main.dart
@@ -0,0 +1,66 @@
+// mod 0 ex00
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+// main()
+// runApp()
+// MyApp widget
+// build()
+// screen UI
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final String message = 'Click me';
+
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                padding: const EdgeInsets.all(12),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurpleAccent[200],
+                  //border: Border.all(color: Colors.white, width: 2),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: const Text(
+                  'A simple text',
+                  style: TextStyle(fontSize: 24, color: Colors.black),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: printButtonPressed,
+                style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                child: Text(
+                  message,
+                  style: TextStyle(color: Colors.deepPurpleAccent[200]),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void printButtonPressed() {
+    debugPrint('Button Pressed');
+  }
+}
+
+// Scaffold --> QMainWindow
+//  body     --> QWidget
+//   Center  --> QVBoxLayout with setAlignment(Qt.AlignCenter)
+//    Column --> QVBoxLayout
+//     Text --> QLabel
+//     ElevatedButton  --> QPushButton
